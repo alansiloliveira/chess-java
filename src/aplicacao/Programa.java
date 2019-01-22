2210b56a80290eca2135b153f175c1d038b650c8
Alan Oliveira <alan.sil.oliveira@gmail.com>
2019-01-22 00:56:15 -0200

    Tabuleiro ja ativo

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,13 @@
 package aplicacao;
 
-import boardgame.Posicao;
-
+import chess.PartidaDeXadrez;
+ 
 public class Programa {
 	
 	public static void main(String[] args) {
 		
-		Posicao pos = new Posicao(3, 5);
-		System.out.println(pos);
+		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
+		UI.fazerTabuleiro(partidaDeXadrez.getPecas());
 	}
 
 }
