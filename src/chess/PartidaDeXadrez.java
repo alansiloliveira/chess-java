e47e29cd99924a73246c84f489a5f8c94bd4b754
Alan Oliveira <alan.sil.oliveira@gmail.com>
2019-01-22 21:51:37 -0200

    Peças no tabuleiro

--- src/chess/PartidaDeXadrez.java
+++ src/chess/PartidaDeXadrez.java
@@ -1,6 +1,9 @@
 package chess;
 
+import boardgame.Posicao;
 import boardgame.Tabuleiro;
+import chess.pieces.Rei;
+import chess.pieces.Torre;
 
 public class PartidaDeXadrez {
 
@@ -8,6 +11,7 @@ public class PartidaDeXadrez {
 	
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		iniciaPartida();
 	}
 	
 	public PecaDeXadrez[][] getPecas(){
@@ -18,5 +22,11 @@ public class PartidaDeXadrez {
 			}
 		}
 		return mat;
+	}
+	
+	private void iniciaPartida() {
+		tabuleiro.posicaoDaPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
+		tabuleiro.posicaoDaPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
+		tabuleiro.posicaoDaPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
 	}
 }
