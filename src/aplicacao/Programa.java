03055e095ad4734b8567d7a325c22b9804fe9a5a
Alan Oliveira <alan.sil.oliveira@gmail.com>
2019-01-23 00:37:18 -0200

    tratando exceçoes

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,32 +1,40 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.PartidaDeXadrez;
 import chess.PecaDeXadrez;
 import chess.PosicaoXadrez;
- 
+
 public class Programa {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		PartidaDeXadrez partidaDeXadrez = new PartidaDeXadrez();
-		
+
 		while (true) {
-		UI.fazerTabuleiro(partidaDeXadrez.getPecas());
-		System.out.println();
-		System.out.print("Origem: ");
-		PosicaoXadrez origem = UI.leiaPosicaoXadrez(sc);
-		
-		System.out.println();
-		System.out.println("Destino: ");
-		PosicaoXadrez destino = UI.leiaPosicaoXadrez(sc);
-		
-		PecaDeXadrez pecaCapturada = partidaDeXadrez.perfomanceMovimentoDaPeca(origem, destino);
+			try {
+				UI.limparTela();
+				UI.fazerTabuleiro(partidaDeXadrez.getPecas());
+				System.out.println();
+				System.out.print("Origem: ");
+				PosicaoXadrez origem = UI.leiaPosicaoXadrez(sc);
+
+				System.out.println();
+				System.out.println("Destino: ");
+				PosicaoXadrez destino = UI.leiaPosicaoXadrez(sc);
+
+				PecaDeXadrez pecaCapturada = partidaDeXadrez.perfomanceMovimentoDaPeca(origem, destino);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
-	
-
 }
- 
